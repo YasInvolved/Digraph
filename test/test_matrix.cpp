a3bbf0d8d1466fdc276c6be942c99b4c6c224b16
YasInvolved <jbaczyk27@gmail.com>
2026-01-28 21:16:11 +0100

    test large matrices

--- test/test_matrix.cpp
+++ test/test_matrix.cpp
@@ -76,6 +76,54 @@ TEST(SIMDMatrix_test, IdentityScalarMulTest1)
 		if (i == j)
 			EXPECT_FLOAT_EQ(val, 5.0f);
 		else
-			EXPECT_FLOAT_EQ(val, 0);
+			EXPECT_FLOAT_EQ(val, 0.0f);
+	}
+}
+
+TEST(SIMDMatrix_test, LargeMatricesScalarMulTest)
+{
+	static constexpr size_t SIZE_LIMIT = 102; // k * k * 100 <= 1 048 576 (1mb)
+	
+	for (size_t s = 1; s < SIZE_LIMIT; s++)
+	{
+		SIMDMatrix mat = SIMDMatrix::Identity(s);
+		mat *= 5;
+
+		EXPECT_EQ(mat.getRowCount(), s);
+		EXPECT_EQ(mat.getColCount(), s);
+
+		for (size_t i = 0; i < mat.getRowCount(); i++)
+		for (size_t j = 0; j < mat.getColCount(); j++)
+		{
+			float val = mat.get(i, j);
+			if (i == j)
+				EXPECT_FLOAT_EQ(val, 5.0f);
+			else
+				EXPECT_FLOAT_EQ(val, 0.0f);
+		}
+	}
+}
+
+TEST(SIMDMatrix_test, LatgeMatricesAdditionTest)
+{
+	static constexpr size_t SIZE_LIMIT = 102; // k * k * 100 <= 1'048'576 (1mb)
+
+	for (size_t s = 1; s < SIZE_LIMIT; s++)
+	{
+		SIMDMatrix mat = SIMDMatrix::Identity(s);
+		mat += mat;
+
+		EXPECT_EQ(mat.getRowCount(), s);
+		EXPECT_EQ(mat.getColCount(), s);
+
+		for (size_t i = 0; i < mat.getRowCount(); i++)
+			for (size_t j = 0; j < mat.getColCount(); j++)
+			{
+				float val = mat.get(i, j);
+				if (i == j)
+					EXPECT_FLOAT_EQ(val, 2.0f);
+				else
+					EXPECT_FLOAT_EQ(val, 0.0f);
+			}
 	}
 }
